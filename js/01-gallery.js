2a4ba1cdeb7d528451fefd7618b390e9619fcf52
AlexKarb <alex.workinfo97@gmail.com>
2021-10-28 16:45:43 +0300

    add 2 page

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+
+
+const galleryRef = document.querySelector(".gallery")
+const instance = basicLightbox.create(`<img src="" width="800" height="600"/>`, {
+    onShow: () => {  window.addEventListener('keydown', escapeCloseModal) },
+    onClose: () => {   window.removeEventListener('keydown', escapeCloseModal)},
+})
+
+
+createGallery(galleryItems);
+
+
+galleryRef.addEventListener('click', (e) => {
+    e.preventDefault()
+    const thisIsImg = e.target.classList.contains('gallery__image');
+
+    if (!thisIsImg) {
+        return
+    };
+
+    instance.element().querySelector('img').src = e.target.dataset.source;
+    instance.show()
+
+    })
+
+
+
+
+function createGallery(array) {
+
+    const markup = array.map(el =>
+     
+        `<li class = "gallery__item">
+            <a class="gallery__link" href="${el.original}">
+                <img
+                class="gallery__image"
+                src="${el.preview}"
+                data-source="${el.original}"
+                alt="${el.description}"
+                />
+            </a>
+        </li>`).join("");
+
+    return galleryRef.innerHTML = markup;
+}
+
+function escapeCloseModal(e) {
+        if (e.key === "Escape") {
+            instance.close() 
+        }
+    }
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+//   {
+//     preview:
+//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
+//     original:
+//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
+//     description: 'Hokkaido Flower',
+//   },
+
+
+
+const galleryRef = document.querySelector(".gallery")
+createGallery(galleryItems);
+
+const gallery = new SimpleLightbox('.gallery__link', { captionDelay: 250 });
+
+
+
+
+function createGallery(array) {
+
+    const markup = array.map(el =>
+       `<li class = "gallery__item">
+            <a class="gallery__link" href="${el.original}">
+                <img
+                class="gallery__image"
+                src="${el.preview}"
+                alt="${el.description}"
+                title="${el.description}"
+                />
+            </a>
+        </li>`).join("");
+
+    return galleryRef.innerHTML = markup;
+}
